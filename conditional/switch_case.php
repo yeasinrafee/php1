4c8a047efff795a9427ca7fe79b63ee31cb719f1
yeasinrafee <mdyeasinrafi7@gmail.com>
2024-04-19 17:49:37 +0600

    switch case

---
+++ conditional/switch_case.php
@@ -0,0 +1,19 @@
+<?php
+
+$age = 18;
+
+switch($age){
+    case 10: 
+        echo "You are 10 years old";
+        break;
+    case 18:
+        echo "You are 18 years old";
+        break;
+    case 89:
+        echo "You are 89 years old";
+        break;
+    default:
+        echo "You never born yet";
+}
+
+?>
